0c3e2ea11da8b6cd8d89170a8bf4a73923ec4ae8
Twodragon <33538534+Twodragon0@users.noreply.github.com>
2023-11-08 15:14:39 +0900

    Update s3_info.py

--- ISMS/s3_info.py
+++ ISMS/s3_info.py
@@ -45,13 +45,20 @@ def get_public_access_block(client, bucket):
     except Exception as e:
         return "Unknown"
 
+def get_bucket_encryption_rules(client, bucket):
+    try:
+        response = client.get_bucket_encryption(Bucket=bucket)
+        return json.dumps(response["ServerSideEncryptionConfiguration"]["Rules"])
+    except Exception as e:
+        return "-"
+
 if __name__ == "__main__":
     client = boto3.client('s3', region_name="ap-northeast-2")
     response = client.list_buckets()
 
     with open('s3_bucket_info.csv', 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(['Name', 'Bucket Encryption', 'Bucket Logging', 'ACL', 'Bucket Policy', 'Public Access Block'])
+        writer.writerow(['Name', 'Bucket Encryption', 'Bucket Logging', 'ACL', 'Bucket Policy', 'Public Access Block', 'Encryption Rules'])
 
         for bucket in response["Buckets"]:
             bucket_name = bucket["Name"]
@@ -60,5 +67,6 @@ if __name__ == "__main__":
             bucket_acl = get_bucket_acl(client, bucket_name)
             bucket_policy = get_bucket_policy(client, bucket_name)
             public_access_block = get_public_access_block(client, bucket_name)
+            encryption_rules = get_bucket_encryption_rules(client, bucket_name)
 
-            writer.writerow([bucket_name, bucket_encryption, bucket_logging, bucket_acl, bucket_policy, public_access_block])
+            writer.writerow([bucket_name, bucket_encryption, bucket_logging, bucket_acl, bucket_policy, public_access_block, encryption_rules])
